b8ab75a52bc9d4a2b36a851823de357f82037ec4
Md Lotfor Rahman <60309981+mlotfor@users.noreply.github.com>
2025-01-24 16:17:21 +0600

    Update gcs.sh

--- gcs.sh
+++ gcs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Update and upgrade system
-sudo apt-mark hold google-cloud-sdk
+sudo apt-mark hold google-cloud-cli google-cloud-cli-anthoscli google-cloud-cli-app-engine-go google-cloud-cli-app-engine-java google-cloud-cli-app-engine-python
+google-cloud-cli-app-engine-python-extras google-cloud-cli-bigtable-emulator google-cloud-cli-cbt google-cloud-cli-cloud-build-local google-cloud-cli-cloud-run-proxy
+google-cloud-cli-datastore-emulator google-cloud-cli-gke-gcloud-auth-plugin google-cloud-cli-kpt google-cloud-cli-local-extract google-cloud-cli-minikube
+google-cloud-cli-nomos google-cloud-cli-package-go-module google-cloud-cli-pubsub-emulator google-cloud-cli-skaffold
+
 sudo apt update && sudo apt upgrade --ignore-hold
 
 # Install wget
