6dc7ce94917fc13c63bba79a1ff9cce3948f7bac
Md Lotfor Rahman <60309981+mlotfor@users.noreply.github.com>
2025-07-15 16:09:10 +0600

    Update gcs.sh

--- gcs.sh
+++ gcs.sh
@@ -12,8 +12,7 @@ sudo apt update && sudo apt upgrade --ignore-hold
 sudo apt install wget -y
 
 # Install Chrome Remote Desktop
-sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo apt install ./chrome-remote-desktop_current_amd64.deb -y
+sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo apt install ./chrome-remote-desktop_current_amd64.deb -y
 
 # Install desktop environment and related packages
 sudo DEBIAN_FRONTEND=noninteractive apt install --assume-yes xfce4 desktop-base dbus-x11 screensaver
